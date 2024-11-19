4683fe4b15d6e2e531c5cb2557b8cebde7e39611
Zeeshan1945 <zeeshann718@gmail.com>
2024-11-19 12:54:35 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // Replace <REPO_NAME> with your GitHub repository name
   server: {
     watch: {
       usePolling: true,
